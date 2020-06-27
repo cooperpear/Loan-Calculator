70dccd1e698a49b951dd76ef012ef32e31bcffae
Cooper Pearson <mptprogesterone@gmail.com>
2020-06-27 17:18:59 -0500

    loader

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.getElementById('loan-form').addEventListener('submit', function (e) {
   //Show loader
   document.getElementById('loading').style.display = 'block';
 
-  setTimeout
+  setTimeout(calculateResults, 1500);
 
 
   e.preventDefault();
@@ -35,6 +35,12 @@ function calculateResults() {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    //Show results
+    document.getElementById('results').style.display = 'block';
+
+    //Hide Loader
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please check your numbers');
   }
@@ -44,6 +50,13 @@ function calculateResults() {
 
 // Show Error
 function showError(error) {
+  //Hide results
+  document.getElementById('results').style.display = 'none';
+
+  //Hide Loader
+  document.getElementById('loading').style.display = 'none';
+
+
   // Create a div
   const errorDiv = document.createElement('div');
 
